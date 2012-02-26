f366fed8c781244c4c7b37ec9f9bdd285f87f62e
Leonardo M. Ramé <martinrame@yahoo.com>
2012-02-26 17:10:43 -0300

    removed demo1 directory

--- demo1/app.js
+++
@@ -1,9 +0,0 @@
-Ext.Loader.setConfig({enabled:true});
-Ext.application({
-    name: 'DEMO',
-    appFolder: 'app',
-    controllers: [],
-    autoCreateViewport: true,
-    launch: function() {
-    }
-});
